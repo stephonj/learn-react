d965e6d7051a5371be15f6e6aa8739cdce28807c
StephonJ <stephonj@email.sc.edu>
2025-10-16 12:34:06 -0400

    Learn React

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Hi Stephon</h1>
+    <h2>Loving This</h2>
+  </React.StrictMode>
+);
+
