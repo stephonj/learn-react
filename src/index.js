07ccd09694d37480aa7af89a3bea484a672d6187
StephonJ <stephonj@email.sc.edu>
2025-10-16 12:34:54 -0400

    Learn React

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <h1>Hi Stephon</h1>
-    <h2>Loving This</h2>
+    <h2>Hello World</h2>
   </React.StrictMode>
 );
 
