111608cd6a7f4c3635760eae8520c560c71688c1
StephonJ <stephonj@email.sc.edu>
2025-10-30 12:29:22 -0400

    Learn React Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,46 @@
 import "./../css/Contact.css";
+import {useState} from "react";
 
 const Contact = () => {
-    return (
-        <main id="contact" className="main-content">
-            <h2>Contact</h2>
+  const [result, setResult] = useState("");
 
-        </main>
-    )
-};
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "8740a021-d716-4fde-aa42-1d8c89573070");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="name" required/>
+        <input type="email" name="email" required/>
+        <textarea name="message" required></textarea>
+
+        <button type="submit">Submit Form</button>
+
+      </form>
+      <span>{result}</span>
+
+    </div>
+  );
+}
 
 export default Contact;
