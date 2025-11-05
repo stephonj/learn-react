07728c172fad4e3a40338c3e63bfac3a9b815092
StephonJ <stephonj@email.sc.edu>
2025-11-05 18:08:16 -0500

    Updated Learn React

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -12,29 +12,29 @@ const Slideshow = () => {
         require.context("../images/slideshow", false, /\.(png|jpe?g|svg$|webp)/)
     );
 
-    const slideForward = () => {
-        /*if(slideIndex < images.length - 1) {
-            setSlideIndex();
+    const slideForward = () =>{
+        /*if(slideIndex < images.length -1){
+            setSlideIndex(slideIndex+1);
         } else {
             setSlideIndex(0);
-        } */
-        setSlideIndex(slideIndex <images.length-1?slideIndex+1:0);
+        }*/
+       setSlideIndex(slideIndex < images.length-1?slideIndex+1:0);
     };
 
     const slideBackward = () => {
         /*if(slideIndex > 0) {
-            setSlideIndex(slideIndex-1);
+            setSlideIndex(slideIndex -1);
         } else {
-            setSlideIndex(images.length-1);
-        } */
-       setSlideIndex(slideIndex > 0?slideIndex-1:images.length-1);
+            setSlideIndex(images.length -1);
+        }*/
+       setSlideIndex(slideIndex > 0?slideIndex-1:images.length -1);
     };
 
     return(
         <section id="slideshow">
             <img src={images[slideIndex]} />
             <a className="arrow" onClick = {slideForward} id="right-arrow" href="#">&gt;</a>
-            <a className="arrow" onClick = {slideBackward} id="left-arrow" href="#">&lt;</a>
+            <a className="arrow" onClick={slideBackward} id="left-arrow" href="#">&lt;</a>
         </section>
     );
 }
